c1d63c862b89d3126fe85c07edc644c65a1e3814
Jarand Ullestad <jarand.ullestad@nationen.no>
2023-03-13 11:03:05 +0100

    Komma i beskrivelser.

--- src/App.svelte
+++ src/App.svelte
@@ -53,14 +53,14 @@ let customData= {
     {#if expand}
     <Hours data={data.symbols.Strøm[elRegion].data[1].hours} />
     {:else}
-    <Symbol title="Nord" data={data.symbols.Strøm.Nord} border=true description="Gjennomsnitt i dag per kWh uten moms." />
+    <Symbol title="Nord" data={data.symbols.Strøm.Nord} border=true description="Gjennomsnitt i dag, per kWh uten moms." />
     <div class="ticker-title" style="margin-left: 10px;" in:fly|local="{{ x: 50, duration: 100, delay:100 }}" out:fly="{{ x: 50, duration: 100 }}">Råvarer</div>
-    <Symbol title="Råolje" data={data.symbols.Råolje} border=false description="Brent olje per fat." />
+    <Symbol title="Råolje" data={data.symbols.Råolje} border=false description="Brent olje, per fat." />
     <Symbol title="Gass" data={data.symbols.Gass} border=true description="Naturgass (TTF, leveranse i april), per MWh." />
-    <Symbol title="Soya" data={data.symbols.Soya} border=true description="Soyabønner per tonn." />
+    <Symbol title="Soya" data={data.symbols.Soya} border=true description="Soyabønner, per tonn." />
     <div class="ticker-title" style="margin-left: 10px;" in:fly|local="{{ x: 50, duration: 100, delay:100 }}" out:fly="{{ x: 50, duration: 100 }}">Makro</div>
-    <Symbol title="KPI" data={customData.kpi} border=false description="Konsumprisindeks endring feb 22 - feb 23." />
-    <Symbol title="Ledighet" data={customData.arb} border=false description="Arbeidsledige i prosent av arbeidsstyrken jan 22." />
+    <Symbol title="KPI" data={customData.kpi} border=false description="Konsumprisindeks endring, feb 22 - feb 23." />
+    <Symbol title="Ledighet" data={customData.arb} border=false description="Arbeidsledige i prosent av arbeidsstyrken, jan 22." />
     {/if}
 {/if}
 
